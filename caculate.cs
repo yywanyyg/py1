baabfc0e9de7c10365068d93c369da4351612274
YYW <yywanyyg@163.com>
2022-03-15 21:15:38 +0800

    caculate.cs is created.

---
+++ caculate.cs
@@ -0,0 +1,19 @@
+public static class caculate
+{
+	public int add(int a,int b)
+	{
+		return a+b;
+	}
+	public int max(int a,int b)
+
+	{
+		if(a>b)
+			return a;
+		else
+			return b;
+	}
+	public int diff(int a,int b)
+	{
+		return a-b;
+	}
+}
